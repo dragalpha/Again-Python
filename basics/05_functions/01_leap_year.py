025d5f92c90437ed68d6ea6a4abab08e7612786f
Santam Sarkar <santamsarkar@gmail.com>
2026-02-18 14:04:05 +0530

    Update 01_leap_year.py

--- basics/05_functions/01_leap_year.py
+++ basics/05_functions/01_leap_year.py
@@ -1,9 +1,10 @@
 def is_leap(year):
     if(year%4==0 and year%100 != 0):
-        print("True")
+        return True
     elif(year%400 == 0):
-        print("True")
-    else:print("False")
+        return True
+    else:
+        return False
     
 year = int(input())
 print(is_leap(year))
